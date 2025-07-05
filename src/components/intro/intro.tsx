93dedefa30182c8e3d19b67ee6052aaa799e61a8
Vasil Despov <60199602+vasildespov@users.noreply.github.com>
2025-07-05 18:06:36 +0300

    feat: add intro illustration

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -1,19 +1,25 @@
 "use client";
 
+import {useMediaQuery} from "@/lib/useMediaQuery";
 import {motion} from "motion/react";
+import Image from "next/image";
 
 export function Intro() {
+  const isMobile = useMediaQuery("(max-width: 1024px)");
+
   return (
-    <section className="flex min-h-dvh">
+    <section className="flex min-h-dvh flex-col items-center justify-center gap-10 px-4 lg:flex-row lg:gap-20 lg:px-8">
       <motion.div
-        initial={{opacity: 0, y: 100}}
+        suppressHydrationWarning
+        initial={{opacity: 0, y: isMobile ? 0 : "25%"}}
         animate={{opacity: 1, y: 0}}
-        transition={{duration: 0.25, delay: 0.25}}
-        className="m-auto flex flex-col items-center justify-center text-center"
+        transition={{delay: 0.25, ease: "easeOut", type: "tween"}}
       >
-        <h1 className="text-8xl font-extrabold tracking-tight">Vasil Despov</h1>
-        <h2 className="text-4xl font-bold">Frontend Developer</h2>
-        <p className="text-muted-foreground mt-5 max-w-3xl">
+        <h2 className="text-2xl font-medium">Frontend Web Developer</h2>
+        <h1 className="text-6xl font-extrabold tracking-tight lg:text-8xl lg:whitespace-nowrap">
+          Vasil Despov
+        </h1>
+        <p className="text-muted-foreground mt-5 max-w-lg">
           I am a&nbsp;
           <span className="text-accent-foreground font-bold">
             Frontend developer
@@ -23,6 +29,14 @@ export function Intro() {
           <br /> I love exploring the latest technologies and am highly flexible
           and adaptive to new challenges.
         </p>
+      </motion.div>
+
+      <motion.div
+        initial={{opacity: 0}}
+        animate={{opacity: 1}}
+        transition={{delay: 0.25, ease: "easeOut", type: "tween"}}
+      >
+        <Image alt="developer" src="/hero.svg" width={500} height={500} />
       </motion.div>
     </section>
   );
