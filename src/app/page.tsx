c58ed8a59c2a51d1fb4c50f780d8d3f8886408d7
Vasil Despov <60199602+vasildespov@users.noreply.github.com>
2025-07-05 12:42:05 +0300

    feat: add smooth scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import {Intro} from "@/components/intro";
+import ReactLenis from "lenis/react";
 
 export default function Home() {
   return (
     <>
+      <ReactLenis root />
       <Intro />
     </>
   );
