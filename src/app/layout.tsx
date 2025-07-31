b3eb2b2668f745ebdbfc01db41fd3d0995e735f3
Vasil Despov <60199602+vasildespov@users.noreply.github.com>
2025-07-31 13:49:50 +0300

    another refactor of intro section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,22 @@
 import "./globals.css";
 import {cn} from "@/lib/utils";
 import type {Metadata} from "next";
-import "next/font/google";
-import {Montserrat} from "next/font/google";
-
-const fontSans = Montserrat({
-  subsets: ["latin"],
-  variable: "--font-sans"
-});
+import {Bebas_Neue, Inter} from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Vasil Despov",
   description: "Developer Portfolio"
 };
+
+const heading = Bebas_Neue({
+  weight: "400",
+  variable: "--font-heading"
+});
+
+const body = Inter({
+  weight: ["400", "700"],
+  variable: "--font-body"
+});
 
 export default function RootLayout({
   children
@@ -21,7 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("antialiased", fontSans.className)}>{children}</body>
+      <body className={cn("antialiased", heading.variable, body.variable)}>
+        {children}
+      </body>
     </html>
   );
 }
--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -2,35 +2,63 @@
 
 import {useGSAP} from "@gsap/react";
 import gsap from "gsap";
+import {SplitText} from "gsap/SplitText";
+import {useRef} from "react";
 
 export function Intro() {
+  const titleRef = useRef(null);
+  const subtitleRef = useRef(null);
+  const copyRef = useRef(null);
+  const buttonsRef = useRef(null);
+
   useGSAP(() => {
-    gsap.from(".intro-content", {
-      y: "10%",
-      opacity: 0,
-      delay: 0.25,
-      duration: 0.25,
-      autoAlpha: 0
+    const {words} = SplitText.create(titleRef.current, {
+      type: "words,chars",
+      charsClass: "transition-opacity hover:opacity-80"
     });
-  }, []);
+    const {chars} = SplitText.create(subtitleRef.current, {
+      type: "chars",
+      mask: "chars"
+    });
+
+    gsap
+      .timeline()
+      .from(words, {
+        y: "500px",
+        stagger: 0.1,
+        delay: 0.1
+      })
+      .from(chars, {
+        x: "-100px"
+      })
+      .to([titleRef.current, subtitleRef.current], {autoAlpha: 1}, 0)
+      .to(copyRef.current, {autoAlpha: 1})
+      .from(buttonsRef.current, {y: "15px"}, "<")
+      .to(buttonsRef.current, {autoAlpha: 1}, "<");
+  });
 
   return (
-    <section className="bg-secondary text-secondary-foreground flex min-h-screen items-center px-5 lg:px-39">
-      <div className="intro-content invisible flex flex-col gap-12">
-        <div className="flex flex-col gap-3">
-          <h2 className="text-muted-foreground text-2xl tracking-widest md:text-4xl">
-            FRONTEND DEVELOPER
-          </h2>
-          <h1 className="text-6xl font-semibold md:text-8xl md:whitespace-nowrap">
-            VASIL DESPOV
-          </h1>
-        </div>
-        <p className="max-w-3xl text-lg font-medium">
-          I am a Frontend Developer with 4+ years of professional experience.
-          With dedication to pixel-perfect details, accessibility and clean
-          code, I specialize in building responsive and performant web
-          applications.
-        </p>
+    <section className="flex min-h-screen flex-col items-center justify-center p-5 lg:p-10">
+      <div className="flex w-full flex-col overflow-hidden lg:items-center">
+        <span ref={subtitleRef} className="intro-subtitle">
+          frontend developer.
+        </span>
+        <h1 ref={titleRef} className="intro-title">
+          vasil despov
+        </h1>
+      </div>
+      <p ref={copyRef} className="invisible w-full lg:text-center">
+        4+ years of professional experience crafting responsive & performant web
+        applications.
+        <br /> Focused on clean code, accessibility, and pixel-perfect
+        implementation.
+      </p>
+      <div
+        ref={buttonsRef}
+        className="invisible mt-5 flex w-full gap-4 lg:justify-center"
+      >
+        <button className="btn">see my work</button>
+        <button className="btn btn-dash">contact me</button>
       </div>
     </section>
   );
